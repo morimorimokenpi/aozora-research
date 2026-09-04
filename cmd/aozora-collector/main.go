d7e1f3f168d7b414cda47fed63b63688ca2c2870
morimorimokenpi <aaa.ys40.y2@gmail.com>
2026-09-05 02:33:00 +0900

    🔨 Implement list 9.6

--- cmd/aozora-collector/main.go
+++ cmd/aozora-collector/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"regexp"
 
 	"github.com/PuerkitoBio/goquery"
 )
@@ -22,11 +23,29 @@ func findEntities(siteURL string) ([]Entity, error) {
 		return nil, err
 	}
 
+	pat := regexp.MustCompile(`.*/cards/([0-9]+)/card([0-9]+).html$`)
 	doc.Find("ol li a").Each(func(n int, elem *goquery.Selection) {
+		token := pat.FindStringSubmatch(elem.AttrOr("href", ""))
+		if len(token) != 3 {
+			return
+		}
+		pageURL := fmt.Sprintf("https://www.aozora.gr.jp/cards/%s/card%s.html", token[1], token[2])
+		author, zipURL := findAuthorAndZIP(pageURL)
+		println(zipURL)
 		println(elem.Text(), elem.AttrOr("href", ""))
 	})
 
 	return nil, nil
+}
+
+func findAuthorAndZIP(siteURL string) (string, string) {
+	doc, err := goquery.NewDocument(siteURL)
+	if err != nil {
+		return "", ""
+	}
+
+	zipURL := ""
+	return zipURL
 }
 
 func main() {
